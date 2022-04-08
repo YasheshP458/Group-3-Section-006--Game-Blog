3e238588877bc1d7e99e0b555689bb9f7b374f7f
Ba Choi <bada@Bas-MacBook-Pro.local>
2022-04-08 03:07:17 -0400

    Game Blog Account Database, hash PW done

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ Chris English, Aaron Reyes
 import os
 import flask
 import requests
+import bcrypt
+from dotenv import load_dotenv, find_dotenv
 
 from flask import Flask, session, abort, redirect
 
@@ -18,51 +20,148 @@ from flask_sqlalchemy import SQLAlchemy
 from oauth2client.contrib.flask_util import UserOAuth2
 import google.oauth2.credentials
 import google_auth_oauthlib.flow
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, EqualTo
+from flask_wtf import FlaskForm
+
+# from flask_bcrypt import Bcrypt
+
+
+from flask_login import (
+    UserMixin,
+    LoginManager,
+    login_required,
+    login_user,
+    logout_user,
+    current_user,
+)
+
+load_dotenv(find_dotenv())
+
 
 app = flask.Flask(__name__)
-# app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
+app.config["BCRYPT_LEVEL"] = 10
+# bcrypt = Bcrypt(app)
 
-''' 
+""" 
 USED FOR CLEANING DATABASE URI, NEED TO TOUCH UP
+"""
+# Point SQLAlchemy to your Heroku database
 
 uri = os.getenv("DATABASE_URL")
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
-'''
 
-app.config["SECRET_KEY"] = "thisissecretkey"
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["GOOGLE_OAUTH2_CLIENT_SECRETS_FILE"] = "client_secret.json"
 # Point SQLAlchemy to Heroku database. Already accounted for postgres -> postgresql
-# app.config["SQLALCHEMY_DATABASE_URI"] = uri
+app.config["SQLALCHEMY_DATABASE_URI"] = uri
 # Gets rid of meaningless warnings
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
-class Database(db.Model):
-    """Movie Data Database"""
-    
-    '''NEED TO CHANGE THIS. SKELETON CODE'''
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String())
-    password = db.Column(db.String())
-    email = db.Column(db.String())
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
 
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(user_id)
+
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    return flask.redirect(flask.url_for("login"))
+
+
+class Users(db.Model, UserMixin):
+    __tablename__ = "Users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(), nullable=False, unique=True)
+    password = db.Column(db.LargeBinary(), nullable=False)
+
+
+db.create_all()
 oauth2 = UserOAuth2(app)
 
-@app.route("/login")
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
     if oauth2.has_credentials():
         return flask.render_template(
-            "main.html", email=oauth2.email, user_id=oauth2.user_id
+            "main.html", username=oauth2.email, user_id=oauth2.user_id
         )
+    elif flask.request.method == "POST":
+        user_form = flask.request.form
+        user_name = user_form["userName"]
+        userPW = user_form["userPW"]
+        user = Users.query.filter_by(username=user_name).first()
+        print(user.password)
+        print(userPW)
+        print(type(userPW))
+        print(userPW.encode())
+        userPW_hash = bcrypt.checkpw(userPW.encode("utf-8"), user.password)
+
+        if userPW_hash:
+            login_user(user)
+
+            return flask.render_template("main.html", username=user.username)
+        else:
+            flask.flash("Can't Find User Info, Please Check Again")
+            return flask.render_template("login.html")
+
     else:
         return flask.render_template("login.html")
 
 
-@app.route("/signup")
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
+    if flask.request.method == "POST":
+        user_form = flask.request.form
+        user_name = user_form["userName"]
+        user_pw = user_form["userPW"]
+        user_repw = user_form["userRePW"]
+        isUN = Users.query.filter_by(username=user_form["userName"]).first()
+
+        if not (user_name or user_pw or user_repw):
+            flask.flash("All Blanks Must be Filled")
+            return flask.redirect(flask.url_for("signup"))
+
+        elif isUN:
+            t = "The user name '"
+            y = "' is already used"
+            flask.flash(t + user_name + y)  # already exist
+            return flask.redirect(flask.url_for("signup"))
+
+        elif len(user_pw) < 4:
+            flask.flash("Password Must be at least 4 digits")  # already exist
+            return flask.redirect(flask.url_for("signup"))
+
+        elif user_pw != user_repw:
+            flask.flash(" Password Not Matching ")
+            return flask.redirect(flask.url_for("signup"))
+
+        else:
+            user_pw_hash = bcrypt.hashpw(user_pw.encode("utf-8"), bcrypt.gensalt())
+            user = Users(username=user_name, password=user_pw_hash)
+
+            db.session.add(user)
+
+            db.session.commit()
+            x = "Sign Up Completed! Login with "
+            flask.flash(x + user_name)
+            return flask.redirect(flask.url_for("login"))
     return flask.render_template("signup.html")
+
+
+# return flask.render_template("signup.html")
 
 
 @app.route("/logout", methods=["GET", "POST"])
